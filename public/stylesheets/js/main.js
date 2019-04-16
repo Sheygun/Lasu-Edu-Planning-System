07fb8218d4b7c48366b9335afc69145fb19be10f
Olabayo Segun Emmanuel <hollasheg@gmail.com>
2019-04-16 18:02:46 -0400

    Updated somes files today

--- public/stylesheets/js/main.js
+++ public/stylesheets/js/main.js
@@ -1,3 +1,81 @@
+let faculty = document.getElementById('faculty');
+let dept = document.getElementById('dept');
+let proceed = document.getElementById('proceeds');
+
+faculty.onchange = function(){
+    if(faculty.value == "science"){
+        dept.disabled = false;
+        dept.options[1].innerHTML = 'COMPUTER SCIENCE';
+        dept.options[2].innerHTML = 'MATHEMATICS';
+        dept.options[3].innerHTML = 'PHYSICS';
+        dept.options[4].innerHTML = 'CHEMISTRY';
+        dept.options[5].innerHTML = 'BIOCHEMISTRY';
+        dept.options[6].innerHTML = 'MICROBIOLOGY';
+        dept.options[7].innerHTML = 'FISHERY';
+        dept.options[8].innerHTML = 'BOTANY';
+        dept.options[9].innerHTML = 'ZOOLOGY';
+
+        dept.options[1].value = 'csc';
+        dept.options[2].value = 'mat';
+        dept.options[3].value = 'phy';
+        dept.options[4].value = 'chm';
+        dept.options[5].value = 'bch';
+        dept.options[6].value = 'mcb';
+        dept.options[7].value = 'fis';
+        dept.options[8].value = 'bot';
+        dept.options[9].value = 'zoo';
+    } else if(faculty.value == "art"){
+        dept.disabled = false;
+        dept.options[1].innerHTML = 'HISTORY AND RELATION';
+        dept.options[2].innerHTML = 'ENGLISH';
+        dept.options[3].innerHTML = 'RELIGION';
+        dept.options[4].innerHTML = 'PHILOSOPHY';
+        dept.options[5].innerHTML = 'FOREIGN LANGUAGE';
+        dept.options[6].innerHTML = 'AFRICAN LANGUAGE';
+        dept.options[7].innerHTML = 'THEATER ARTS';
+        dept.options[8].style.visibility = 'hidden';
+        dept.options[9].style.visibility = 'hidden';
+
+        dept.options[1].value = '';
+        dept.options[2].value = '';
+        dept.options[3].value = '';
+        dept.options[4].value = '';
+        dept.options[5].value = '';
+        dept.options[6].value = '';
+        dept.options[7].value = '';
+        dept.options[8].value = '';
+        dept.options[9].value = '';
+    }
+    else{
+        dept.disabled = true;
+    }
+
+}
+dept.onchange = function(){
+    if(dept.value != "dept"){
+        if(faculty.value == "science" && dept.value == "csc"){
+            proceed.disabled = false;
+                proceed.onclick = function(){
+                    window.location.replace('/minipage');
+                }
+        }else{
+            proceed.disabled = true;
+        }
+
+    }else{
+          proceed.disabled = true;
+    }
+}
+
+
+
+
+
+
+
+
+
+
 function myFunction(){
     alert("goal");
 }
@@ -8,96 +86,181 @@ function printIn(){
 }
 
 function scienceComplex(){
-    document.getElementById('height').innerHTML = "100cm";
-    document.getElementById('width').innerHTML = "100cm";
-    document.getElementById('fan').innerHTML = "100cm";
-    document.getElementById('aircon').innerHTML = "100cm";
-    document.getElementById('chair').innerHTML = "100cm";
-    document.getElementById('ratio').innerHTML = "1:100";
-    document.getElementById('ratio2').innerHTML = "1:6";
-    document.getElementById('ratio3').innerHTML = "1:3";
-    document.getElementById('ratio4').innerHTML = "1:1";
-    document.getElementById('ratio5').innerHTML = "1:0";
+    document.getElementById('fan').innerHTML = "24";
+    document.getElementById('aircon').innerHTML = "5";
+    document.getElementById('chair').innerHTML = "375";
+        let chair = document.getElementById('chair').innerHTML;
+        let csclevel1 = document.getElementById('csclevel1').innerHTML;
+        let csclevel2 = document.getElementById('csclevel2').innerHTML;
+        let csclevel3 = document.getElementById('csclevel2').innerHTML;
+        let csclevel4 = document.getElementById('csclevel2').innerHTML;
+        let csclevel5 = document.getElementById('csclevel2').innerHTML;
+
+            document.getElementById('headers').innerHTML = "Science Complex I"
+    let ratio = (csclevel1/5)/5 + ":" + (chair/5)/5;
+    let ratio2 = (csclevel2/5) + ":" + (chair/5);
+    let ratio3 = (csclevel2/5) + ":" + (chair/5);
+    let ratio4 = (csclevel2/5) + ":" + (chair/5);
+    let ratio5 = (csclevel2/5) + ":" + (chair/5);
+
+    document.getElementById('ratio').innerHTML = ratio;
+    document.getElementById('ratio2').innerHTML = ratio2;
+    document.getElementById('ratio3').innerHTML = ratio3;
+    document.getElementById('ratio4').innerHTML = ratio4;
+    document.getElementById('ratio5').innerHTML = ratio5;
 }
 function scienceComplex2(){
-    document.getElementById('height').innerHTML = "200cm";
-    document.getElementById('width').innerHTML = "200cm";
-    document.getElementById('fan').innerHTML = "200cm";
-    document.getElementById('aircon').innerHTML = "200cm";
-    document.getElementById('chair').innerHTML = "200cm";
-    document.getElementById('ratio').innerHTML = "1:100";
-    document.getElementById('ratio2').innerHTML = "1:6";
-    document.getElementById('ratio3').innerHTML = "1:3";
-    document.getElementById('ratio4').innerHTML = "1:1";
-    document.getElementById('ratio5').innerHTML = "1:0";
+    document.getElementById('fan').innerHTML = "24";
+    document.getElementById('aircon').innerHTML = "5";
+    document.getElementById('chair').innerHTML = "375";
+        let chair = document.getElementById('chair').innerHTML;
+        let csclevel1 = document.getElementById('csclevel1').innerHTML;
+        let csclevel2 = document.getElementById('csclevel2').innerHTML;
+        let csclevel3 = document.getElementById('csclevel2').innerHTML;
+        let csclevel4 = document.getElementById('csclevel2').innerHTML;
+        let csclevel5 = document.getElementById('csclevel2').innerHTML;
+
+            document.getElementById('headers').innerHTML = "Science Complex II"
+    let ratio = (csclevel1/5)/5 + ":" + (chair/5)/5;
+    let ratio2 = (csclevel2/5) + ":" + (chair/5);
+    let ratio3 = (csclevel2/5) + ":" + (chair/5);
+    let ratio4 = (csclevel2/5) + ":" + (chair/5);
+    let ratio5 = (csclevel2/5) + ":" + (chair/5);
+
+    document.getElementById('ratio').innerHTML = ratio;
+    document.getElementById('ratio2').innerHTML = ratio2;
+    document.getElementById('ratio3').innerHTML = ratio3;
+    document.getElementById('ratio4').innerHTML = ratio4;
+    document.getElementById('ratio5').innerHTML = ratio5;
 }
+
 function scienceRoom1(){
-    document.getElementById('height').innerHTML = "3500cm";
-    document.getElementById('width').innerHTML = "4300cm";
-    document.getElementById('fan').innerHTML = "12300cm";
-    document.getElementById('aircon').innerHTML = "8600cm";
-    document.getElementById('chair').innerHTML = "3400cm";
-    document.getElementById('ratio').innerHTML = "1:100";
-    document.getElementById('ratio2').innerHTML = "1:6";
-    document.getElementById('ratio3').innerHTML = "1:3";
-    document.getElementById('ratio4').innerHTML = "1:1";
-    document.getElementById('ratio5').innerHTML = "1:0";
+    document.getElementById('fan').innerHTML = "24";
+    document.getElementById('aircon').innerHTML = "5";
+    document.getElementById('chair').innerHTML = "150";
+        let chair = document.getElementById('chair').innerHTML;
+        let csclevel1 = document.getElementById('csclevel1').innerHTML;
+        let csclevel2 = document.getElementById('csclevel2').innerHTML;
+        let csclevel3 = document.getElementById('csclevel2').innerHTML;
+        let csclevel4 = document.getElementById('csclevel2').innerHTML;
+        let csclevel5 = document.getElementById('csclevel2').innerHTML;
+
+            document.getElementById('headers').innerHTML = "Science Room I"
+    let ratio = (csclevel1/5)/5 + ":" + (chair/5)/5;
+    let ratio2 = (csclevel2/5) + ":" + (chair/5);
+    let ratio3 = (csclevel2/5) + ":" + (chair/5);
+    let ratio4 = (csclevel2/5) + ":" + (chair/5);
+    let ratio5 = (csclevel2/5) + ":" + (chair/5);
+
+    document.getElementById('ratio').innerHTML = ratio;
+    document.getElementById('ratio2').innerHTML = ratio2;
+    document.getElementById('ratio3').innerHTML = ratio3;
+    document.getElementById('ratio4').innerHTML = ratio4;
+    document.getElementById('ratio5').innerHTML = ratio5;
 }
+
 function scienceRoom2(){
-    document.getElementById('height').innerHTML = "6400cm";
-    document.getElementById('width').innerHTML = "5300cm";
-    document.getElementById('fan').innerHTML = "3200cm";
-    document.getElementById('aircon').innerHTML = "1200cm";
-    document.getElementById('chair').innerHTML = "1100cm";
-    document.getElementById('ratio').innerHTML = "1:100";
-    document.getElementById('ratio2').innerHTML = "1:6";
-    document.getElementById('ratio3').innerHTML = "1:3";
-    document.getElementById('ratio4').innerHTML = "1:1";
-    document.getElementById('ratio5').innerHTML = "1:0";
+    document.getElementById('fan').innerHTML = "24";
+    document.getElementById('aircon').innerHTML = "5";
+    document.getElementById('chair').innerHTML = "375";
+        let chair = document.getElementById('chair').innerHTML;
+        let csclevel1 = document.getElementById('csclevel1').innerHTML;
+        let csclevel2 = document.getElementById('csclevel2').innerHTML;
+        let csclevel3 = document.getElementById('csclevel2').innerHTML;
+        let csclevel4 = document.getElementById('csclevel2').innerHTML;
+        let csclevel5 = document.getElementById('csclevel2').innerHTML;
+
+            document.getElementById('headers').innerHTML = "Science Room 2"
+    let ratio = (csclevel1/5)/5 + ":" + (chair/5)/5;
+    let ratio2 = (csclevel2/5) + ":" + (chair/5);
+    let ratio3 = (csclevel2/5) + ":" + (chair/5);
+    let ratio4 = (csclevel2/5) + ":" + (chair/5);
+    let ratio5 = (csclevel2/5) + ":" + (chair/5);
+
+    document.getElementById('ratio').innerHTML = ratio;
+    document.getElementById('ratio2').innerHTML = ratio2;
+    document.getElementById('ratio3').innerHTML = ratio3;
+    document.getElementById('ratio4').innerHTML = ratio4;
+    document.getElementById('ratio5').innerHTML = ratio5;
 }
 function scienceRoom3(){
-    document.getElementById('height').innerHTML = "500cm";
-    document.getElementById('width').innerHTML = "600cm";
-    document.getElementById('fan').innerHTML = "1000cm";
-    document.getElementById('aircon').innerHTML = "300cm";
-    document.getElementById('chair').innerHTML = "300cm";
-    document.getElementById('ratio').innerHTML = "1:100";
-    document.getElementById('ratio2').innerHTML = "1:6";
-    document.getElementById('ratio3').innerHTML = "1:3";
-    document.getElementById('ratio4').innerHTML = "1:1";
-    document.getElementById('ratio5').innerHTML = "1:0";
+    document.getElementById('fan').innerHTML = "24";
+    document.getElementById('aircon').innerHTML = "5";
+    document.getElementById('chair').innerHTML = "375";
+        let chair = document.getElementById('chair').innerHTML;
+        let csclevel1 = document.getElementById('csclevel1').innerHTML;
+        let csclevel2 = document.getElementById('csclevel2').innerHTML;
+        let csclevel3 = document.getElementById('csclevel2').innerHTML;
+        let csclevel4 = document.getElementById('csclevel2').innerHTML;
+        let csclevel5 = document.getElementById('csclevel2').innerHTML;
+
+            document.getElementById('headers').innerHTML = "Science Room 3"
+    let ratio = (csclevel1/5)/5 + ":" + (chair/5)/5;
+    let ratio2 = (csclevel2/5) + ":" + (chair/5);
+    let ratio3 = (csclevel2/5) + ":" + (chair/5);
+    let ratio4 = (csclevel2/5) + ":" + (chair/5);
+    let ratio5 = (csclevel2/5) + ":" + (chair/5);
+
+    document.getElementById('ratio').innerHTML = ratio;
+    document.getElementById('ratio2').innerHTML = ratio2;
+    document.getElementById('ratio3').innerHTML = ratio3;
+    document.getElementById('ratio4').innerHTML = ratio4;
+    document.getElementById('ratio5').innerHTML = ratio5;
 }
 function computerLab(){
-    document.getElementById('height').innerHTML = "4300cm";
-    document.getElementById('width').innerHTML = "2300cm";
-    document.getElementById('fan').innerHTML = "23000cm";
-    document.getElementById('aircon').innerHTML = "54300cm";
-    document.getElementById('chair').innerHTML = "4300cm";
-    document.getElementById('ratio').innerHTML = "1:80";
-    document.getElementById('ratio2').innerHTML = "1:6";
-    document.getElementById('ratio3').innerHTML = "1:3";
-    document.getElementById('ratio4').innerHTML = "1:1";
-    document.getElementById('ratio5').innerHTML = "1:0";
+    document.getElementById('fan').innerHTML = "24";
+    document.getElementById('aircon').innerHTML = "5";
+    document.getElementById('chair').innerHTML = "120";
+        let chair = document.getElementById('chair').innerHTML;
+        let csclevel1 = document.getElementById('csclevel1').innerHTML;
+        let csclevel2 = document.getElementById('csclevel2').innerHTML;
+        let csclevel3 = document.getElementById('csclevel2').innerHTML;
+        let csclevel4 = document.getElementById('csclevel2').innerHTML;
+        let csclevel5 = document.getElementById('csclevel2').innerHTML;
+
+            document.getElementById('headers').innerHTML = "Computer Laboratory"
+    let ratio = (csclevel1/5)/5 + ":" + (chair/5)/5;
+    let ratio2 = (csclevel2/5) + ":" + (chair/5);
+    let ratio3 = (csclevel2/5) + ":" + (chair/5);
+    let ratio4 = (csclevel2/5) + ":" + (chair/5);
+    let ratio5 = (csclevel2/5) + ":" + (chair/5);
+
+    document.getElementById('ratio').innerHTML = ratio;
+    document.getElementById('ratio2').innerHTML = ratio2;
+    document.getElementById('ratio3').innerHTML = ratio3;
+    document.getElementById('ratio4').innerHTML = ratio4;
+    document.getElementById('ratio5').innerHTML = ratio5;
 }
 function benson(){
-    document.getElementById('height').innerHTML = "34300cm";
-    document.getElementById('width').innerHTML = "4300cm";
-    document.getElementById('fan').innerHTML = "54000cm";
-    document.getElementById('aircon').innerHTML = "53300cm";
-    document.getElementById('chair').innerHTML = "6700cm";
-    document.getElementById('ratio').innerHTML = "2:80";
-    document.getElementById('ratio2').innerHTML = "3:6";
-    document.getElementById('ratio3').innerHTML = "2:3";
-    document.getElementById('ratio4').innerHTML = "4:1";
-    document.getElementById('ratio5').innerHTML = "2:434";
+    document.getElementById('fan').innerHTML = "24";
+    document.getElementById('aircon').innerHTML = "5";
+    document.getElementById('chair').innerHTML = "375";
+        let chair = document.getElementById('chair').innerHTML;
+        let csclevel1 = document.getElementById('csclevel1').innerHTML;
+        let csclevel2 = document.getElementById('csclevel2').innerHTML;
+        let csclevel3 = document.getElementById('csclevel2').innerHTML;
+        let csclevel4 = document.getElementById('csclevel2').innerHTML;
+        let csclevel5 = document.getElementById('csclevel2').innerHTML;
+
+            document.getElementById('headers').innerHTML = "Benson Hall";
+    let ratio = (csclevel1/5)/5 + ":" + (chair/5)/5;
+    let ratio2 = (csclevel2/5) + ":" + (chair/5);
+    let ratio3 = (csclevel2/5) + ":" + (chair/5);
+    let ratio4 = (csclevel2/5) + ":" + (chair/5);
+    let ratio5 = (csclevel2/5) + ":" + (chair/5);
+
+    document.getElementById('ratio').innerHTML = ratio;
+    document.getElementById('ratio2').innerHTML = ratio2;
+    document.getElementById('ratio3').innerHTML = ratio3;
+    document.getElementById('ratio4').innerHTML = ratio4;
+    document.getElementById('ratio5').innerHTML = ratio5;
 }
 
 
 
 
 function clears(){
-    document.getElementById('height').innerHTML = "";
-    document.getElementById('width').innerHTML = "";
+    document.getElementById('headers').innerHTML = "";
     document.getElementById('fan').innerHTML = "";
     document.getElementById('aircon').innerHTML = "";
     document.getElementById('chair').innerHTML = "";
@@ -110,45 +273,26 @@ function clears(){
 
 // ================================================================ staffs =========================================
 
-function sscienceComplex(){
-    document.getElementById('sheight').innerHTML = "34300cm";
-    document.getElementById('swidth').innerHTML = "4300cm";
-    document.getElementById('sfan').innerHTML = "54000cm";
-    document.getElementById('saircon').innerHTML = "53300cm";
-    document.getElementById('schair').innerHTML = "6700cm";
-    document.getElementById('sratio').innerHTML = "2:80";
-    document.getElementById('sratio2').innerHTML = "3:6";
-    document.getElementById('stable').innerHTML = "2:434";
-}
-function sscienceComplex2(){
-    document.getElementById('sheight').innerHTML = "4546cm";
-    document.getElementById('swidth').innerHTML = "4645cm";
-    document.getElementById('sfan').innerHTML = "4654cm";
-    document.getElementById('saircon').innerHTML = "345cm";
-    document.getElementById('schair').innerHTML = "546cm";
-    document.getElementById('sratio').innerHTML = "2:30";
-    document.getElementById('sratio2').innerHTML = "3:56";
-    document.getElementById('stable').innerHTML = "4:656";
-}
-function scomputerLab(){
-    document.getElementById('sheight').innerHTML = "43300cm";
-    document.getElementById('swidth').innerHTML = "64600cm";
-    document.getElementById('sfan').innerHTML = "564000cm";
-    document.getElementById('saircon').innerHTML = "345600cm";
-    document.getElementById('schair').innerHTML = "34500cm";
-    document.getElementById('sratio').innerHTML = "3:320";
-    document.getElementById('sratio2').innerHTML = "3:655";
-    document.getElementById('stable').innerHTML = "2:764";
-}
+function checkNow(){
+    document.getElementById('titleBar').innerHTML = "100 Level Students";
+    let cscTeachingStaff = document.getElementById('cscTeachingStaff').innerHTML;
+    let cscNonteachingStaff = document.getElementById('cscNonteachingStaff').innerHTML;
+        let csclevel1 = document.getElementById('csclevel1').innerHTML;
+        let csclevel2 = document.getElementById('csclevel2').innerHTML;
+        let csclevel3 = document.getElementById('csclevel2').innerHTML;
+        let csclevel4 = document.getElementById('csclevel2').innerHTML;
+        let csclevel5 = document.getElementById('csclevel2').innerHTML;
 
+        let ratio = (csclevel1/5) + ":" + (cscTeachingStaff/5);
+        let ratio2 = (csclevel2/5) + ":" + (cscNonteachingStaff/5);
+
+        document.getElementById('sratio').innerHTML = ratio;
+        document.getElementById('sratio2').innerHTML = ratio2;
+
+
+}
 function sclears(){
-    document.getElementById('sheight').innerHTML = "";
-    document.getElementById('swidth').innerHTML = "";
-    document.getElementById('sfan').innerHTML = "";
-    document.getElementById('saircon').innerHTML = "";
-    document.getElementById('schair').innerHTML = "";
+    document.getElementById('titleBar').innerHTML = "";
     document.getElementById('sratio').innerHTML = "";
-    document.getElementById('sratio2').innerHTML = "";
-    document.getElementById('stable').innerHTML = "";
+        document.getElementById('sratio2').innerHTML = "";
 }
-
